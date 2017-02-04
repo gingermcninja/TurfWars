26e77526657c85217301c997bebeb2dd67363769
Paul McGrath <paul_a_mcgrath@hotmail.com>
2017-02-04 14:41:35 -0500

    Initial Commit

---
+++ TurfWars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TurfWars
+//
+//  Created by Paul McGrath on 04/02/2017.
+//  Copyright © 2017 Paul McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
